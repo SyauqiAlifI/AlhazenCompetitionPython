80a1e1908d0248ddd048f54841ee68316e15d51b
SyauqiAlifI <vstudio776@gmail.com>
2022-08-27 15:32:15 +0700

    oke

---
+++ task1.py
@@ -0,0 +1,82 @@
+from operator import truediv
+import turtle
+
+# Make a turtle project with formulas, variable and function
+half_serogka = 22.5
+serong_kanan = 45.0
+go_right = 90.0
+sharp_right = go_right + half_serogka
+
+# Welcome
+def welcome():
+    print("""
+        1 for hexagon
+        2 for pentagon
+        3 for rectangle
+    """)
+    inp = input("Choose shape : ")
+    my_turtle = turtle.Turtle()
+    my_turtle.shape("turtle")
+    if inp == "1":
+        draw_hexagon(my_turtle)
+    elif inp == "2":
+        draw_pentagon(my_turtle)
+    elif inp == "3":
+        draw_rectangle(my_turtle)
+
+# Hexagone (6)
+def draw_hexagon(my_turtle):
+    my_size = float(input("Input amount of forward : "))
+    i = 1
+    while i <= 8:
+        my_turtle.forward(float(my_size))
+        my_turtle.right(serong_kanan)
+        my_turtle.forward(float(my_size))
+        i += 1
+    isDone = input("Done? (Boolean) : ")
+    if isDone == "True" or isDone == "true":
+        print("Bye!")
+    else:
+        draw_hexagon()
+
+# Pentagon (5)
+def draw_pentagon(my_turtle):
+    my_size = float(input("Input amount of forward : "))
+    in_size = my_size + 20
+    i = 1
+    my_turtle.right(half_serogka)
+    # \
+    my_turtle.forward(in_size)
+    my_turtle.right(sharp_right)
+    # /
+    my_turtle.forward(my_size)
+    my_turtle.right(serong_kanan)
+    # _
+    my_turtle.forward(my_size)
+    my_turtle.right(serong_kanan)
+    # \
+    my_turtle.forward(my_size)
+    my_turtle.right(sharp_right)
+    # /
+    my_turtle.forward(in_size)
+    isDone = input("Done? (Boolean) : ")
+    if isDone == "True" or isDone == "true":
+        print("Bye!")
+    else:
+        draw_pentagon()
+
+# rectangle (4)
+def draw_rectangle(my_turtle):
+    my_size = float(input("Input amount of forward : "))
+    i = 1
+    while i <= 4:
+        my_turtle.right(go_right)
+        my_turtle.forward(my_size)
+        i += 1
+    isDone = input("Done? (Boolean) : ")
+    if isDone == "True" or isDone == "true":
+        print("Bye!")
+    else:
+        draw_pentagon()
+
+welcome()
---
+++ task2.py
@@ -0,0 +1,7 @@
+def welcome():
+    in_name = str(input("Input Your Name: "))
+    in_age = input("Input your Age: ")
+    in_place = str(input("Input your home place: "))
+    in_num = input("Input your phone nunmber: ")
+    print(in_name, in_age, in_place, in_num)
+    print("Thank u for register to this page")
