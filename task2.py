5354dc6c8724e5b3713995e1a09216e0b52d141d
SyauqiAlifI <vstudio776@gmail.com>
2022-08-28 16:41:27 +0700

    fixed

--- task2.py
+++ task2.py
@@ -1,8 +1,34 @@
+
 def welcome():
-    in_name = str(input("Input Your Name: "))
+    in_name = input("Input Your Name: ")
+    check_inp_str(in_name)
+
     in_age = input("Input your Age: ")
-    in_place = str(input("Input your home place: "))
+    check_inp_int(in_age)
+
+    in_place = input("Input your home place: ")
+    check_inp_str(in_place)
+
     in_num = input("Input your phone nunmber: ")
+    check_inp_int(in_num)
+
     print(in_name, in_age, in_place, in_num)
     print("Thank u for register to this page")
+
+def check_inp_str(input):
+    try:
+        is_ok = int(input)
+        print("Wrong input")
+        exit()
+    except ValueError:
+        print("Continue")
+
+def check_inp_int(input):
+    try:
+        is_ok = int(input)
+        print("Continue")
+    except ValueError:
+        print("Wrong input")
+        exit()
+
 welcome()
